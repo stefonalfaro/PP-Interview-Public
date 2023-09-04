2646c1741db1985c8bcae60fa04f2cb2723bcbc1
Stefon Alfaro <stefon.alfaro@pioneeringprogrammers.com>
2023-09-04 09:12:45 -0600

    Generic Account model.

---
+++ Models/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_BE_Interview.Models
+{
+    public class SimplifiedAccount
+    {
+        public const string CLASS_NAME = "Account";
+        public const string CONTAINER_NAME = "Account";
+
+        public string ClassName { get; set; } = CLASS_NAME;
+        public string accountName { get; set; }
+        public string Status { get; set; }
+    }
+}
